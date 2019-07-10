07adb319f3dd61a356391e2159eccdf6d40dc4db
Mike <mikeyobviously@gmail.com>
2019-07-10 16:37:57 -0700

    more basic setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, Link } from 'react-router-dom';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>SockOp</h1>
+        <span>Login</span>
       </header>
+      <Switch>
+        <Route exact path="/signup" render={({history}) =>
+          <SignUpPage history={history} />
+        } />
+        <Route exact path="/login" component={LoginPage} />
+      </Switch>
+
+        <h2>We're doing stuff here</h2>
     </div>
   );
 }
---
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignUpPage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default SignUpPage;
