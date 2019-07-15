a9559d90f2a21791f0bab18d12b291247c69f2cd
Mike <mikeyobviously@gmail.com>
2019-07-15 10:53:24 -0700

    image map component added

--- src/pages/DropoffPage/DropoffPage.jsx
+++ src/pages/DropoffPage/DropoffPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hotspot from '../../components/Hotspot';
 import DropoffForm from '../../components/DropoffForm';
+import Map from '../../components/Map';
 
 const DropoffPage = (props) => (
   props.hotspotSelected ?
@@ -10,15 +11,19 @@ const DropoffPage = (props) => (
     />
     </div>
   :
-    <div className='hotspot-list'>
-      {props.hotspots.map((hotspot, idx) =>
-        <Hotspot 
-          selectHotspot = {props.selectHotspot}
-          hotspot = {hotspot}
-          key = {hotspot.location}
-        />
-      )}
-    </div>
+    <>
+      <Map />
+      <div className='hotspot-list'>
+        
+        {props.hotspots.map((hotspot, idx) =>
+          <Hotspot 
+            selectHotspot = {props.selectHotspot}
+            hotspot = {hotspot}
+            key = {hotspot.location}
+          />
+        )}
+      </div>
+    </>
 );
 
 export default DropoffPage;
