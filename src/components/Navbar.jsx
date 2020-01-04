5a98935281f301c8f828ee75ec2031d3f6adaf43
Mike <mikeyobviously@gmail.com>
2020-01-03 16:15:25 -0800

    misc CSS updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,13 +11,9 @@ const Navbar = (props) => {
       <Link to='/view' className='NavBar-link'>View Drops</Link>
       &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
 
-      <Link to='/admin' className='NavBar-link'>Admin
-      </Link>
-      &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-
       <Link to='' className='NavBar-link' onClick={props.handleLogout}>LOG OUT</Link>
       &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-      <span className='NavBar-welcome'>WELCOME, {props.user.name}</span>
+      <span className='NavBar-welcome'>Welcome, {props.user.name}</span>
     </div>
     :
     <div>
